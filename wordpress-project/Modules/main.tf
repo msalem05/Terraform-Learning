05744fd93e7b812a738111c424d9eff2d7987087
msalem05 <salemmohammed571@gmail.com>
2025-07-30 20:18:05 +0100

    set up vpc module

--- wordpress-project/Modules/main.tf
+++ wordpress-project/Modules/main.tf
@@ -3,9 +3,11 @@ module "ec2" {
     instance_type = var.instance_type
     vpc_id = module.vpc.vpc_id
     subnet_id = module.vpc.public_subnet_id
-    
+
 }
 
 module "vpc" {
     source = "./Modules/VPC"
+    
+
 }
